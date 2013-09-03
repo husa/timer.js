aff7549e71de76915626b390e9dc1539c6bfc4f1
Yuriy Husnay <husnaj23@gmail.com>
2013-09-03 19:43:45 +0300

    Identation converted to spaces

--- node/node_modules/timer/timer.js
+++ node/node_modules/timer/timer.js
@@ -1,145 +1,145 @@
 (function () {
-	'use strict';
+    'use strict';
 var Timer = function (options) {
 
-	var defaultOptoins = {
-		tick    : 1,
-		onstart : function () {},
-		onstop  : function () {},
-		onpause : function () {},
-		onend   : function () {},
-		ontick  : function () {}
-	};
+    var defaultOptoins = {
+        tick    : 1,
+        onstart : function () {},
+        onstop  : function () {},
+        onpause : function () {},
+        onend   : function () {},
+        ontick  : function () {}
+    };
 
-	//extend options with defaultOptoins
-	if (typeof(options) === 'object') {
-		for (var prop in defaultOptoins) {
-			if (!options.hasOwnProperty(prop)) {
-				options[prop] = defaultOptoins[prop];
-			}
-		}
-	} else {
-		options = defaultOptoins;
-	}
+    //extend options with defaultOptoins
+    if (typeof(options) === 'object') {
+        for (var prop in defaultOptoins) {
+            if (!options.hasOwnProperty(prop)) {
+                options[prop] = defaultOptoins[prop];
+            }
+        }
+    } else {
+        options = defaultOptoins;
+    }
 
-	// private variables
-	var that = {
-			id : +new Date(),
-			duration : 0,
-			status : 'initialized',
-			start : 0
-		},
-		timeout,
-		interval;
+    // private variables
+    var that = {
+            id : +new Date(),
+            duration : 0,
+            status : 'initialized',
+            start : 0
+        },
+        timeout,
+        interval;
 
-	function start(duration) {
-		if (!+duration && !that.duration) {
-			return end();
-		} else {
-			duration *= 1000;
-		}
+    function start(duration) {
+        if (!+duration && !that.duration) {
+            return end();
+        } else {
+            duration *= 1000;
+        }
 
-		if (timeout && that.status === 'started') return this;
+        if (timeout && that.status === 'started') return this;
 
-		that.duration = duration = that.duration === 0 ? duration : that.duration;
-		timeout = setTimeout(end, duration);
-		that.start = +new Date();
-		if (options.ontick !== defaultOptoins.ontick) {
-			interval = setInterval(function() {
-				options.ontick( getDuration() );
-			}, +options.tick * 1000);
-		}
+        that.duration = duration = that.duration === 0 ? duration : that.duration;
+        timeout = setTimeout(end, duration);
+        that.start = +new Date();
+        if (options.ontick !== defaultOptoins.ontick) {
+            interval = setInterval(function() {
+                options.ontick( getDuration() );
+            }, +options.tick * 1000);
+        }
         that.status = 'started';
-		options.onstart();
-		return this;
-	}
+        options.onstart();
+        return this;
+    }
 
-	function stop() {
-		clearTimeout(timeout);
-		clearInterval(interval);
+    function stop() {
+        clearTimeout(timeout);
+        clearInterval(interval);
         that.duration = 0;
         that.status = 'stopped';
-		options.onstop();
-		return this;
-	}
+        options.onstop();
+        return this;
+    }
 
-	function pause() {
-		that.duration = that.duration - (+new Date() - that.start);
-		clearTimeout(timeout);
-		clearInterval(interval);
+    function pause() {
+        that.duration = that.duration - (+new Date() - that.start);
+        clearTimeout(timeout);
+        clearInterval(interval);
         that.status = 'paused';
-		options.onpause();
-		return this;
-	}
+        options.onpause();
+        return this;
+    }
 
-	function end() {
-		clearTimeout(timeout);
-		clearInterval(interval);
+    function end() {
+        clearTimeout(timeout);
+        clearInterval(interval);
         that.duration = 0;
         that.status = 'finished';
-		options.onend();
-		return this;
-	}
+        options.onend();
+        return this;
+    }
 
-	function getStatus() {
-		return that.status;
-	}
+    function getStatus() {
+        return that.status;
+    }
 
-	function getDuration() {
-		return Math.round((that.duration - (+new Date() - that.start)) / 1000);
-	}
+    function getDuration() {
+        return Math.round((that.duration - (+new Date() - that.start)) / 1000);
+    }
 
-	function on(option, value) {
-		if (typeof(option) !== 'string' || typeof(value) !== 'function') return;
-		if (option.indexOf('on') !== 0) {
-			option = 'on' + option;
-		}
-		if (options.hasOwnProperty(option)) {
-			options[option] = value;
-		}
-		return this;
-	}
+    function on(option, value) {
+        if (typeof(option) !== 'string' || typeof(value) !== 'function') return;
+        if (option.indexOf('on') !== 0) {
+            option = 'on' + option;
+        }
+        if (options.hasOwnProperty(option)) {
+            options[option] = value;
+        }
+        return this;
+    }
 
-	function off(option) {
-		if (typeof(option) !== 'string') {
-			return;
-		} else {
-			option = option.toLowerCase();
-		}
-		if (option === 'all') {
-			options = defaultOptoins;
-		} else {
-			if (option.indexOf('on') !== 0) {
-				option = 'on' + option;
-			}
-			if (options.hasOwnProperty(option)) {
-				options[option] = defaultOptoins[option];
-			}
-		}
-		return this;
-	}
+    function off(option) {
+        if (typeof(option) !== 'string') {
+            return;
+        } else {
+            option = option.toLowerCase();
+        }
+        if (option === 'all') {
+            options = defaultOptoins;
+        } else {
+            if (option.indexOf('on') !== 0) {
+                option = 'on' + option;
+            }
+            if (options.hasOwnProperty(option)) {
+                options[option] = defaultOptoins[option];
+            }
+        }
+        return this;
+    }
 
-	return {
-		start 		: start,
-		stop 		: stop,
-		pause 		: pause,
-		on 			: on,
-		off			: off,
-		getStatus 	: getStatus,
-		getDuration : getDuration
-	};
+    return {
+        start       : start,
+        stop        : stop,
+        pause       : pause,
+        on          : on,
+        off         : off,
+        getStatus   : getStatus,
+        getDuration : getDuration
+    };
 };
 
-	//export Timer for Node or as global variable in browser
-	var root = this;
+    //export Timer for Node or as global variable in browser
+    var root = this;
 
-	if (typeof(exports) !== 'undefined') {
-		if (typeof(module) !== 'undefined' && module.exports) {
-			exports = module.exports = Timer;
-		}
-		exports.Timer = Timer;
-	} else {
-		root.Timer = Timer;
-	}
+    if (typeof(exports) !== 'undefined') {
+        if (typeof(module) !== 'undefined' && module.exports) {
+            exports = module.exports = Timer;
+        }
+        exports.Timer = Timer;
+    } else {
+        root.Timer = Timer;
+    }
 
 }).call(this);
