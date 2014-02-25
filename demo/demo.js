bf77bd47608ed007167ce6d70866b415ba6b49fe
Yuriy Husnay <husnaj23@gmail.com>
2014-02-25 10:18:00 +0200

    demo page added

---
+++ demo/demo.js
@@ -0,0 +1,52 @@
+(function($) {
+    $(function() {
+        var $play  = $('.play'),
+            $pause = $('.pause'),
+            $stop  = $('.stop'),
+            $time  = $('.time-input'),
+            $timer = $('.timer');
+
+        var timer = new Timer({
+            onstart : function(sec) {
+                $timer.text(sec);
+            },
+            ontick  : function(sec) {
+                $timer.text(sec);
+            },
+            onpause : function() {
+                $timer.text('pause');
+            },
+            onstop  : function() {
+                $timer.text('stop');
+            },
+            onend   : function() {
+                $timer.text('end');
+            }
+        });
+
+        $play.on('click', function() {
+            var time = $time.val();
+            if (!time) return;
+            if (isNaN(time)) {
+                alert('Please input valid number');
+                return;
+            }
+
+            timer.start(time);
+        });
+
+        $pause.on('click', function() {
+            if (timer.getStatus() === 'started') {
+                timer.pause();
+            }
+        });
+
+        $stop.on('click', function() {
+            if (/started|paused/.test(timer.getStatus())) {
+                timer.stop();
+            }
+        });
+
+
+    });
+}(jQuery));
--- example.js
+++
@@ -1,9 +0,0 @@
-var Timer = require('./timer');
-
-var t = new Timer({
-    'ontick' : function(sec){
-        console.log(this.getDuration());
-    }
-});
-
-t.start(10);
