830947a7844a19c3f1129e7aaf31db485a0ea81c
Yuriy Husnay <husnaj23@gmail.com>
2023-05-09 00:03:45 +0300

    refactor: run prettier+eslint on source, fix eslint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,12 @@ module.exports = {
   root: true,
   env: {
     es6: true,
+    browser: true,
+    node: true,
   },
   globals: {},
   extends: ["eslint:recommended", "prettier"],
+  ignorePatterns: ["dist/**/*.*"],
   overrides: [
     {
       files: ["test/specs/*.js"],
